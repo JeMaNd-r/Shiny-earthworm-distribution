35223edc6237087952850b5237bbe2bb033824ed
JeMaNd-r <53231180+JeMaNd-r@users.noreply.github.com>
2022-09-05 15:57:13 +0200

    Add species-specific data

--- R/Prepare_data.R
+++ R/Prepare_data.R
@@ -7,19 +7,36 @@
 #                                       #
 #- - - - - - - - - - - - - - - - - - - -#
 
+# set working directory to source file location
+getwd()
+
 # load packages
 library(tidyverse)
 
 # load data
 load("../data/SDM_stack_bestPrediction_binary_Crassiclitellata.RData") #species_stack
 load("../data/SDM_stack_future_richness_change_Crassiclitellata.RData") #average_stack
+load("../data/SDM_stack_future_species_Crassiclitellata.RData") #future_stack
+load("../data/SDM_Uncertainty_Crassiclitellata.RData") #uncertain_df
 
 head(average_stack)
 head(species_stack)
+head(future_stack)
+head(uncertain_df)
 
-
-
-
+# richness df
 richness_df <- average_stack
 
 save(richness_df, file="Earthworms_in_Europe/richness_df.RData")
+
+# species df
+species_df <- full_join(species_stack, future_stack)
+head(species_df)
+
+save(species_df, file="Earthworms_in_Europe/species_df.RData")
+
+# uncertainty df
+save(uncertain_df, file="Earthworms_in_Europe/uncertain_df.RData")
+
+
+
